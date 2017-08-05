acbacdc28cc346b15f29bc760c43fa293582d39a
heartless007 <barryblando@gmail.com>
2017-08-05 20:00:12 +0800

    Update: Set winning score 07/05/2017

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,3 @@
-/*
-GAME RULES:
-
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he wishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
-*/
-
 var scores, 
     roundScore, 
     activePlayer,
@@ -71,7 +60,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     //get input value for winning score
     var input =  document.querySelector('.final-score').value;
     var winningScore;
-    
+
     // Undefined, 0, null, empty string " " are COERCED to false
     // Anything else is COERCED to true\
     winningScore = input ? input : 100;
