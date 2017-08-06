2ef64fb368790543e86f535a1d059107c864a620
heartless007 <barryblando@gmail.com>
2017-08-06 21:36:56 +0800

    Update: Removed one rule 08/06/2017

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,8 +25,8 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
   */
   if(gamePlaying) {
     // 1. Random Number
-    var dice1 = Math.floor(Math.random() *6) + 1;
-    var dice2 = Math.floor(Math.random() *6) + 1;
+    var dice1 = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
     // 2. Display the  result
     document.getElementById('dice-1').style.display = 'block';
     document.getElementById('dice-2').style.display = 'block';
